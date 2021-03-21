b04b1d3e28c16e2b15f701535b41d7aefddba1ed
Amitabh  Baruah <amitabhyo@gmail.com>
2021-03-21 13:01:49 +0530

    data structure updated to two-dimension

--- src/components/App.js
+++ src/components/App.js
@@ -12,15 +12,16 @@ const App = () => {
     //Keep Track of the directory
     const [DirectoryStack, setDirectoryStack] = useState(Array(['17772']));
     const [CurrentDirectoryList, setCurrentDirectoryList] = useState([]);
-    const [DirectoryPointer, setDirectoryPointer] = useState([]);
+    const [DirectoryPointerOffset, setDirectoryPointerOffset] = useState(0);
+    const [DirectoryPointerRow, setDirectoryPointerRow] = useState(0);
 
 
     const [selected, setselected] = useState('Amitabh')
 
 
     useEffect(() => {
-        setDirectoryPointer(DirectoryStack.length);
-        exploreFolder(DirectoryStack[DirectoryStack.length - 1][0])
+        let temp = traceDirectories(DirectoryStack[0]);
+        setCurrentDirectoryList(temp)
     }, [DirectoryStack]);
 
     const sideBarSelect = (tab) => {
@@ -29,66 +30,79 @@ const App = () => {
     }
     const exploreFolder = (dir) => {
         if (directoryTree['home'][dir]) {
-            if (DirectoryStack[DirectoryStack.length - 1][0] === dir) {
-                let dirStackTemp = DirectoryStack;
-                dirStackTemp[dirStackTemp.length - 1] = [dir];
-                setDirectoryStack(dirStackTemp); // Handling case if Sidebar click and again clicked on same parent directory
-            }
-            else {
-                setDirectoryStack([...DirectoryStack, [dir]])
-            }
-            // console.log(directoryTree['home'][dir])
-            setCurrentDirectoryList(directoryTree['home'][dir]['child']);
-        } else {
-            let pointer = DirectoryPointer;
-            if (DirectoryPointer !== DirectoryStack.length) {
-                let dirTemp = DirectoryStack;
-                dirTemp = dirTemp.slice(0, DirectoryPointer);
-                setDirectoryPointer(DirectoryStack.length);
-                pointer = DirectoryStack.length;
-            }
+            //Means it is a home directory
+            //Append the DirectoryStack
+            let row = DirectoryPointerRow;
             let dirStackTemp = DirectoryStack;
-            let currentTrack = dirStackTemp[pointer - 1];
-            currentTrack.push(dir);
-            dirStackTemp[pointer - 1] = currentTrack;
+            if (row !== dirStackTemp.length - 1) {
+                row = dirStackTemp.length - 1;
+                setDirectoryPointerRow(row);
+                setDirectoryPointerOffset(0);
+            }
+            if (DirectoryStack[row][0] === dir) {
+                dirStackTemp[row] = [dir];
+                setDirectoryStack(dirStackTemp);
+            } else {
+                row += 1;
+                setDirectoryPointerOffset(0);
+                setDirectoryPointerRow(row);
+                dirStackTemp.push([dir]);
+                setDirectoryStack(dirStackTemp);
+            }
+            //Trace Directory Items
+            let temp = traceDirectories(dirStackTemp[row]);
+            setCurrentDirectoryList(temp)
+        } else {
+            let offset = DirectoryPointerOffset;
+            let row = DirectoryPointerRow;
+            let dirStackTemp = DirectoryStack;
+            if (offset !== dirStackTemp[row].length-1) {
+                dirStackTemp[row] =  dirStackTemp[row].slice(0,offset+1);
+                offset = dirStackTemp[row].length-1;
+                setDirectoryPointerOffset(offset);
+            }
+            dirStackTemp[row].push(dir);
+            offset += 1;
+            setDirectoryPointerOffset(offset);
             setDirectoryStack(dirStackTemp);
-            let temp = traceDirectories(currentTrack);
-            //Iteratively find the directories of current context.
 
-            if (temp)
-                setCurrentDirectoryList(temp)
+            //Trace Directory Items
+            let temp = traceDirectories(dirStackTemp[row]);
+            setCurrentDirectoryList(temp)
         }
     }
     const traceDirectories = (traceArr) => {
-        console.log(traceArr)
         let temp = [];
         for (let i = 0; i < traceArr.length; i++) {
             if (i === 0) {
                 temp = directoryTree['home'][traceArr[i]]['child'];
-            }else{
-                if(temp[traceArr[i]])
-                temp = temp[traceArr[i]]['child'];
+            } else {
+                if (temp[traceArr[i]])
+                    temp = temp[traceArr[i]]['child'];
             }
         }
         return temp;
     }
     const handleGoBack = () => {
-        let dirTemp = DirectoryStack[DirectoryPointer - 1];
-        if(DirectoryStack[0].length > 1 || DirectoryStack.length>1){
-            dirTemp.pop();
+        let row = DirectoryPointerRow;
+        let offset = DirectoryPointerOffset;
+        let dirStackTemp = DirectoryStack.map(e => e);
+        if (offset === 0) {
+            row -= 1;
+            setDirectoryPointerRow(row);
+            offset = dirStackTemp[row].length - 1;
+            setDirectoryPointerOffset(offset);
+            let temp = traceDirectories(dirStackTemp[row]);
+            setCurrentDirectoryList(temp)
+        } else {
+            let path = dirStackTemp[row].slice(0, offset);
+            offset = offset - 1;
+            setDirectoryPointerOffset(offset);
+            let temp = traceDirectories(path);
+            setCurrentDirectoryList(temp)
         }
-        if (dirTemp.length === 0) {
-            if (DirectoryPointer > 0) {
-                let temp  = DirectoryStack;
-                temp.pop();
-                setDirectoryPointer(DirectoryPointer - 1) // Decrement only if Directory  stack's ELEMENT is 0 length
-                setDirectoryStack(temp)
-            }
-        }
-        console.log(DirectoryPointer)
-        // let temp = dirTemp.length === 0  ?  traceDirectories(DirectoryStack[DirectoryPointer-1]):traceDirectories(dirTemp);
-        // if (temp)
-        //     setCurrentDirectoryList(temp)
+
+
     }
     return (
         <div >
@@ -98,7 +112,7 @@ const App = () => {
             </div>
             <div className="pusher">
                 <Nav onGoBack={handleGoBack} />
-          <Explorer directories={CurrentDirectoryList} exploreFolder={exploreFolder} />
+                <Explorer directories={CurrentDirectoryList} exploreFolder={exploreFolder} />
             </div>
         </div>
     )
